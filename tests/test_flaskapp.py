cfbe8e1d66e23d3db180f56c11bf3b8774a777fb
ronald <VELZR01@mac0740008.home>
2020-05-22 12:23:13 +0200

    added tests

--- tests/test_flaskapp.py
+++ tests/test_flaskapp.py
@@ -1,25 +1,36 @@
-import unittest,json
+import unittest
 
-from main import app
+def hello():
+    return "Hello World!"
+
+
+def multiply(x,y):
+    return x * y
+
+
+def touppercase(s):
+    return s.upper()
 
 
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
+        pass
 
-    def test_unauthorized(self):
-        response = self.app.get('/authorized')
-        # Check if the request fails with authorization error
-        self.assertEqual(response._status_code,401,'Unauthorized access to page without login')
 
     def test_multiply(self):
-        response = self.app.get('/multiply?x=5&y=7')
-        resp = json.loads(response.data.decode())
-        self.assertEqual(resp['answer'],35,'Multiply endpoint failed known answer 7*5 = 35')
+        x = 10
+        y = 12
 
-    # TODO DEFINE TWO MORE TESTS ON THE END POINTS
+        answer = multiply(x,y)
 
+        self.assertEqual(answer, 120)
+
+    def test_upper(self):
+        string = "test"
+
+        answer = touppercase(string)
+
+        self.assertEqual(answer, "TEST")
 
 if __name__ == '__main__':
     unittest.main()
